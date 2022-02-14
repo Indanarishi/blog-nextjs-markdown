cef13b1da545645b90940b81d478877a1a40038d
Jundi <indana6969@gmail.com>
2022-02-14 14:22:14 +0700

    displaying single post and sort post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,9 +4,21 @@ import matter from 'gray-matter'
 import { marked } from 'marked'
 import Link from 'next/link'
 
-export default function PostPage({frontmadtter: {title, date, cover_image}, slug, content}) {
+export default function PostPage({frontmatter: {title, date, cover_image}, slug, content}) {
     return (
-        <div>{title}</div>
+        <>
+            <Link href='/'>
+                <a className="btn btn-back">Go Back</a>
+            </Link>
+            <div className="card card-page">
+                <h1 className="post-title">{title}</h1>
+                <div className="post-date">Posted on {date}</div>
+                <img src={cover_image} alt={title} />
+                <div className="post-body">
+                    <div dangerouslySetInnerHTML={{__html: marked(content)}}></div>
+                </div>
+            </div>
+        </>
     )
 }
 
@@ -28,11 +40,11 @@ export async function getStaticPaths() {
 export async function getStaticProps({params: {slug}}) {
     const markdownWithMeta = fs.readFileSync(path.join('posts', slug + '.md'), 'utf-8')
 
-    const { data: frontmadtter, content } = matter(markdownWithMeta)
+    const { data: frontmatter, content } = matter(markdownWithMeta)
 
     return {
         props: {
-            frontmadtter,
+            frontmatter,
             slug,
             content
         }
