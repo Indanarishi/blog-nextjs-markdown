6e14118201ac25faf4684b1c17d1a1b03f2c4d3c
Jundi <indana6969@gmail.com>
2022-02-14 14:07:34 +0700

    add post page and generate paths

---
+++ pages/blog/[slug].js
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { marked } from 'marked'
+import Link from 'next/link'
+
+export default function PostPage({frontmadtter: {title, date, cover_image}, slug, content}) {
+    return (
+        <div>{title}</div>
+    )
+}
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync(path.join('posts'))
+
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace('.md', '')
+        }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params: {slug}}) {
+    const markdownWithMeta = fs.readFileSync(path.join('posts', slug + '.md'), 'utf-8')
+
+    const { data: frontmadtter, content } = matter(markdownWithMeta)
+
+    return {
+        props: {
+            frontmadtter,
+            slug,
+            content
+        }
+    }
+}
